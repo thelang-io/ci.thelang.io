1fb3bb67393e6b1e36699eeaf330c38b3c22deb3
Aaron Delasy <aaron@thelang.io>
2023-01-08 03:25:41 +0200

    Add echo endpoint

---
+++ controllers/echo.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+
+const examplePath = path.join(__dirname, '..', 'data', 'example.html')
+const standardHeadersPath = path.join(__dirname, '..', 'data', 'standard-headers.txt')
+const exampleData = fs.readFileSync(examplePath, 'utf8').trim()
+const standardHeaders = fs.readFileSync(standardHeadersPath, 'utf8')
+  .trim()
+  .split('\n')
+  .map(it => it.trim())
+  .filter(it => it !== '')
+
+module.exports = async (req, res) => {
+  const statusCode = parseInt(req.query.status) || 200
+  const sleepTime = parseInt(req.query.sleep) || 0
+  const headerKeys = Object.keys(req.headers)
+    .filter(it => !standardHeaders.includes(it))
+
+  if (sleepTime > 0) {
+    await new Promise((resolve) => {
+      setTimeout(resolve, sleepTime)
+    })
+  }
+
+  for (const headerKey of headerKeys) {
+    res.setHeader(headerKey, req.headers[headerKey])
+  }
+
+  res.status(statusCode)
+
+  if (Object.prototype.hasOwnProperty.call(req.query, 'example')) {
+    res.setHeader('content-type', 'text/html')
+    res.end(exampleData)
+  } else if (Object.prototype.hasOwnProperty.call(req.query, 'ip')) {
+    res.end(req.ip)
+  } else {
+    res.end(req.body)
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const helmet = require('helmet')
+const echoController = require('../controllers/echo')
+const wrapMiddleware = require('../middlewares/wrap')
 
 const router = express.Router()
+
+router.all(
+  '/echo',
+  bodyParser.text({ type: () => true }),
+  wrapMiddleware(echoController)
+)
 
 router.all('*', helmet(), (_req, res) => {
   res.redirect(301, 'https://thelang.io/?utm_source=ci')
